83def8193a58e726eb239ef0dd028b43b4f81d1b
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-14 01:27:43 -0400

    Se hizo el route path con el navigate para cuando la persona no este autentificada

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -6,6 +6,8 @@ export const AuthRoutes = () => {
     <Routes>
         <Route path = "login" element= {LoginPage} /> 
         <Route path = 'register' element = {RegisterPage} /> 
+        
+        <Route path ='/*' element= {<Navigate to="/auth/login"/>} /> 
     </Routes>
   )
 }
