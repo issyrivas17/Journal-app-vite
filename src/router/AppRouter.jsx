b69aab400320ea061d64b4166a74f2fc639e2923
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-15 10:41:44 -0400

    Se actualizo el app router y el JournalApp se le paso el appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,16 @@
 import { Route,Routes } from "react-router-dom"; 
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
 
 export const AppRouter = () => {
     return (
        <Routes>
 
         {/*Login y registro*/}
-       <Route/> 
+       <Route path="/auth/*" element={<AuthRoutes/>}/> 
 
        {/*JournalApp*/} 
-       <Route/> 
+       <Route path="/*" element={<JournalRoutes/>}/>  
        </Routes> 
     )
 }
