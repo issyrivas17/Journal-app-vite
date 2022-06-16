2b195386a4d34b1cd520ebd727784ad894496c83
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-15 23:11:54 -0400

    Se envuelve el AppRouter con AppTheme para poner el theme en toda la app

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,12 +1,13 @@
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTheme"
 
 
 export const JournalApp = () => {
   return (
-    <> 
+    <AppTheme> 
     
     <AppRouter/> 
     
-    </>
+    </AppTheme>
   )
 }
