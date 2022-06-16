6a6df60894d59c60b7bf9754a4389a5e2a381109
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-15 23:10:13 -0400

    Se pasa el purpleTheme al ThemeProvider en el archivo AppTheme

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,10 +1,11 @@
 import { CssBaseline } from '@mui/material'
 import React, { Children } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { purpleTheme } from './purpleTheme'
 
 export const AppTheme = ({Children}) => {
   return (
-    <ThemeProvider theme={theme}> 
+    <ThemeProvider theme={purpleTheme}> 
     
     <CssBaseline/> 
     {Children}
