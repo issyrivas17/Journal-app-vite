613c0030a922d17fb7e38e13936f516a598b4f71
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-15 23:40:29 -0400

    Se instalo y configuro correctamente todo

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,13 @@
+import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
 
 export const JournalPage = () => {
   return (
-    <Typography>JournalPage</Typography> 
+    <> 
+    <Typography variant='h1'>JournalPage</Typography> 
+    <MailOutline/> 
+    </>
   )
 }
  
