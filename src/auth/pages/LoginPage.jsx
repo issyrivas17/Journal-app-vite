8fc58ef01ce39872dee9fa4913947c011a880f2a
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-17 15:49:24 -0400

    Se paso un mt:2 para que quedara la caja de texto espaciada con el login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -41,7 +41,7 @@ export const LoginPage = () => {
       /> 
     </Grid>
 
-    <Grid container spacing={2} sx={{mb:2}}>
+    <Grid container spacing={2} sx={{mb:2, mt:2}}>
       <Grid item xs={12} sm={6}>
       <Button variant="contained" fullWidth> 
       Login 
