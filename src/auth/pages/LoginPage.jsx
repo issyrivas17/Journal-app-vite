b81f03d4fa7aa3b268f4e1b9a3fa16c1589f9f41
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-17 15:44:01 -0400

    El boton me sale pegado a la caja de texto de la contraseña

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -39,7 +39,7 @@ export const LoginPage = () => {
       /> 
     </Grid>
 
-    <Grid container spacing = {2} sx={{mb:2}}>
+    <Grid container spacing={2} sx={{mb:2}}>
       <Grid item xs={12}>
       <Button variant="contained" > 
       Login 
