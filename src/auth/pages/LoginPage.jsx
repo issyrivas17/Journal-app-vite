caacff87b7d7d68bf3afc46f04c702192ba09d6a
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-20 00:07:32 -0400

    Se elimina el principio del login page para pasarle el layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -5,21 +5,7 @@ import {Google} from '@mui/icons-material'
 
 export const LoginPage = () => {
   return (
-  <Grid 
- container
- spacing={0} 
- direction = "column"
- alignItems = "center" 
- justifyContent="center" 
- sx= {{ minHeight: '100vh', backgroundColor:'primary.main', padding: 4}}
- >
-   <Grid item 
-   className='box-shadow'
-   xs= {3} 
-   sx= {{backgroundColor : 'white', padding: 3, borderRadius:2}}>  
-
-   <Typography variant='h5' sx={{mb:1}}>Login</Typography> 
-
+  
    <form>
     <Grid container > 
 
