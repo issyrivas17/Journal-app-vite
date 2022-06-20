ed15905eaeedec7c89104782c60364ce1c6b87e2
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-20 01:39:27 -0400

    se termino el navbar

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,18 +1,32 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material'
-import {MenuOutlined} from '@mui/icons-material'
+import { AppBar, IconButton, Toolbar, Typography,Grid} from '@mui/material'
+import {LogoutOutlined, MenuOutlined} from '@mui/icons-material'
 import React from 'react' 
 
-export const NavBar = () => {
+export const NavBar = ({ drawerWidth = 240 }) => {
   return (
    <AppBar
    position='fixed'
-   sx={{ }}
+   sx={{
+    width: {sm:`calc(100% - ${drawerWidth} px)`},
+    ml: {sm: `${drawerWidth}px`}
+    }}
    >
 
     <Toolbar>
-        <IconButton>
+        <IconButton 
+        color= 'inherit'
+        edge= "start"
+        sx={{mr:2,display:{sm: 'none'}}} 
+        >
             <MenuOutlined/> 
         </IconButton>
+        <Grid container direction= 'row' justifyContent= 'space-between' alignItems='center'>  
+        <Typography variant='h6' noWrap component='div'>JournalApp</Typography> 
+        <IconButton color='error'> 
+            <LogoutOutlined/> 
+        </IconButton>
+
+        </Grid>
     </Toolbar>
 
    </AppBar>
