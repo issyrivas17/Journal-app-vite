3a49d373c48f777a7132a46ac77b019f29680e21
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-20 00:22:44 -0400

    estaba pasado mal el auth/register

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -51,7 +51,7 @@ export const LoginPage = () => {
     </Grid>
 
     <Grid container direction='row' justifyContent= 'end'> 
-    <Link component= {RouterLink} color = 'inherit' to="/auth/registr"> 
+    <Link component= {RouterLink} color = 'inherit' to="/auth/register"> 
     Crear una cuenta 
     </Link>
    
