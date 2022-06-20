96397a70303d2fad63644e0bf23c73b89138ead4
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-20 00:20:09 -0400

    Se modificio el tamaño de la pantalla mediana ¿

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -14,7 +14,11 @@ export const AuthLayout = ({children,title= ''}) => {
       <Grid item 
       className='box-shadow'
       xs= {3} 
-      sx= {{backgroundColor : 'white', padding: 3, borderRadius:2}}>  
+      sx= {{
+        width : {md: 450},
+        backgroundColor : 'white', 
+        padding: 3, 
+        borderRadius:2}}>  
    
       <Typography variant='h5' sx={{mb:1}}>{title}</Typography> 
 
