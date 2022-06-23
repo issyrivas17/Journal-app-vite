3e310c5c72a1608349e5d979e538a43c5e433209
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-23 12:47:51 -0400

    Se le aplico un borderRadius al nothingSelect

--- src/auth/views/NothingSelect.jsx
+++ src/auth/views/NothingSelect.jsx
@@ -10,7 +10,7 @@ export const NothingSelect = () => {
     direction = "column"
     alignItems = "center" 
     justifyContent="center" 
-    sx= {{ minHeight: 'calc (100vh -110px)', backgroundColor:'primary.main'}}
+    sx= {{ minHeight: 'calc (100vh -110px)', backgroundColor:'primary.main',borderRadius:5}}
    > 
    <Grid item xs={12}>
     <StarOutline sx={{fontSize: 100, color: 'white'}}/>  
