05967a69dbde0d10a7e57cb20f984746841a7558
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-23 12:34:02 -0400

    Se pasan dos grid y se agrega starOutline con icons-material

--- src/auth/views/NothingSelect.jsx
+++ src/auth/views/NothingSelect.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { Grid } from '@mui/material'
+import {StarOutline} from '@mui/icons-material' 
+
 
 export const NothingSelect = () => {
   return (
-    <div>Nothing Select views </div>
-  )
+    <Grid
+    container
+    spacing={0} 
+    direction = "column"
+    alignItems = "center" 
+    justifyContent="center" 
+    sx= {{ minHeight: 'calc (100vh -110px)', backgroundColor:'primary.main'}}
+   > 
+   <Grid item xs={12}>
+    <StarOutline/> 
+
+   </Grid>
+
+   </Grid> 
+   
+  
+
+
+
+
+
+  ) 
 }
