80cb22f4076f3414c532013c22a1a19d13edd4dd
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-24 00:26:42 -0400

    Se hizo el boton flotante vamos a probarlo

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import { NoteView } from '../../auth/views/NoteView.jsx'
 
 import { JournalLayout } from '../layout/JournalLayout.jsx'
@@ -11,8 +13,20 @@ export const JournalPage = () => {
     <JournalLayout> 
    
 {/*   <NothingSelect/>  */}
-    
     <NoteView/> 
+
+    <IconButton
+    sx={{
+    color:'white',
+    background: 'error.main',
+    ':hover':{backgroundColor:'error.main',opacity:0.9},
+    position:'fixed', 
+    right:50,
+    botton:50
+  }}
+    >
+    <AddOutlined sx={{fontSize:30}}/> 
+    </IconButton>
     </JournalLayout>
   )
 }
