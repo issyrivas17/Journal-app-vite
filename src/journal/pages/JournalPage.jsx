0cde5692cb656b9d013f85350fce267011d06b0a
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-24 00:32:15 -0400

    No se ve el boton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,6 +2,7 @@
 import { AddOutlined } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
 import { NoteView } from '../../auth/views/NoteView.jsx'
+import { NothingSelect } from '../../auth/views/NothingSelect.jsx'
 
 import { JournalLayout } from '../layout/JournalLayout.jsx'
 
@@ -12,21 +13,22 @@ export const JournalPage = () => {
   return (
     <JournalLayout> 
    
-{/*   <NothingSelect/>  */}
-    <NoteView/> 
-
-    <IconButton
+  <NothingSelect/>  
+   
+    <IconButton 
+    size='large'
     sx={{
     color:'white',
     background: 'error.main',
     ':hover':{backgroundColor:'error.main',opacity:0.9},
     position:'fixed', 
     right:50,
-    botton:50
+    bottom:50 
   }}
     >
     <AddOutlined sx={{fontSize:30}}/> 
     </IconButton>
+
     </JournalLayout>
   )
 }
